fc8430fc77563417ad9a6f41200329f4c65156f3
brandonliu <brandonliu52@gmail.com>
2015-09-02 13:56:25 -0700

    Update CorrelationTool.R

--- CorrelationTool.R
+++ CorrelationTool.R
@@ -4,7 +4,7 @@
 load('/Users/BrandonLiu/Documents/Stanford/Data\ Analysis/RelationalDatabaseProject/deleteome_raw_data.RData')
 
 # <<<<<<<<< NEED TO MAKE THE FOLLOWING FUNCTION CALLS >>>>>>>>>
-
+# Note: some of the data is not formatted well, so it is necessary to modify it
 # 2. Need to call fixColnames to reformat the columns
 # -------------------------------------------------
 colnames(deleteome$FC) = sapply(colnames(deleteome$FC), function(x) fixColnames(x))
@@ -16,23 +16,23 @@ make.unique(colnames(deleteome$p.value))
 make.unique(colnames(deleteome$FC))
 
 
-# Convert all characters to upper case
-
+# Convert all characters to upper case, reformats the loaded data appropriately
 colnames(deleteome$FC) = toupper(colnames(deleteome$FC))
 colnames(deleteome$p.value) = colnames(deleteome$FC)
 
 library(org.Sc.sgd.db)
 
-
+# Converts systematic yeast names to standard names
 convertToStandard <- function(systematicNames) {
   return(select(org.Sc.sgd.db,keys=systematicNames,keytype='ORF',columns='GENENAME')$GENENAME)
 }
 
+# Converts yeast standard names to systematic names
 convertToSystematic <- function(standardNames) {
   return(select(org.Sc.sgd.db,keys=standardNames,keytype='GENENAME',columns='ORF')$ORF)
 }
 
-# Holstege
+# Performs correlations based on data from the Holstege lab deleteome
 HolstegeCorrelations <- function(firstGene, geneSet) {
   print(paste('Printing correlations against: ', firstGene))
   firstGeneName = convertToSystematic(standardNames = firstGene)
